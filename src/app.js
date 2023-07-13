a2d35511f5768194198d34c23691985c8587ce2a
thisisyoussef <youssefahmed@ufl.edu>
2023-07-13 04:08:04 +0300

    updated institution page design

--- src/app.js
+++ src/app.js
@@ -7,6 +7,85 @@ app.use(bodyParser.json());
 
 const PORT = process.env.PORT || 3007;
 
+const CSS = `
+  body {
+    font-family: Arial, sans-serif;
+    margin: 0;
+    padding: 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: #f0f0f0;
+    color: #333;
+  }
+
+  h1 {
+    color: #444;
+  }
+
+  p {
+    color: #444;
+    line-height: 1.5;
+  }
+
+  button {
+    padding: 15px 30px;
+    font-size: 20px;
+    background-color: #007BFF;
+    color: white;
+    border: none;
+    border-radius: 5px;
+    cursor: pointer;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  button:disabled {
+    background-color: #999;
+    cursor: not-allowed;
+  }
+
+  .spinner {
+    display: none;
+    margin-right: 10px;
+  }
+
+  button:disabled .spinner {
+    display: inline-block;
+  }
+
+  .container {
+    width: 90%;
+    max-width: 600px;
+    margin: 0 auto;
+    padding: 20px;
+    box-sizing: border-box;
+    text-align: center;
+  }
+
+  @media (min-width: 600px) {
+    .container {
+      padding: 20px;
+    }
+  }
+`;
+
+const JavaScript = `
+  function pay() {
+    // Change button state to indicate processing
+    const payButton = document.getElementById('payButton');
+    payButton.disabled = true;
+
+    // Simulate a delay before redirect
+    setTimeout(() => {
+      // Redirect back to the app using the deep link
+      const deepLink = 'custom.flutterbooksample.com://transaction/${transactionNumber}';
+      window.location.href = deepLink;
+    }, 2000);
+  }
+`;
+
 app.get('/paymentProcess', (req, res) => {
   // Generate transaction details (dummy data)
   const transactionNumber = '123456789';
@@ -16,90 +95,33 @@ app.get('/paymentProcess', (req, res) => {
     return res.status(400).send('Invalid transaction details.');
   }
 
-  // Render the financial institution page
-  res.send(`
+  const HTML = `
     <html>
       <head>
         <title>Financial Institution</title>
-        <style>
-          body {
-            font-family: Arial, sans-serif;
-            margin: 0;
-            padding: 0;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            height: 100vh;
-            background-color: #f0f0f0;
-            color: #333;
-          }
-
-          h1 {
-            color: #444;
-          }
-
-          p {
-            color: #444;
-            line-height: 1.5;
-          }
-
-          button {
-            padding: 15px 30px;
-            font-size: 20px;
-            background-color: #007BFF;
-            color: white;
-            border: none;
-            border-radius: 5px;
-            cursor: pointer;
-          }
-
-          button:hover {
-            background-color: #0056b3;
-          }
-
-          .container {
-            max-width: 600px;
-            margin: 0 auto;
-            padding: 20px;
-            box-sizing: border-box;
-          }
-
-          @media (max-width: 600px) {
-            .container {
-              padding: 10px;
-            }
-          }
-        </style>
+        <style>${CSS}</style>
       </head>
       <body>
-        <header>
-          <h1>Transaction Details</h1>
-        </header>
         <section class="container">
+          <header>
+            <h1>Transaction Details</h1>
+          </header>
           <p><strong>Transaction Number:</strong> ${transactionNumber}<br>
           This is your unique identifier for this transaction.</p>
           <p><strong>Amount to Pay:</strong> $${amountToPay}<br>
           This is the total amount due for the transaction.</p>
-          <button id="payButton" onclick="pay()">Complete Transaction</button>
+          <button id="payButton" onclick="pay()">
+            <span class="spinner">&#x1F310;</span>
+            Complete Transaction
+          </button>
         </section>
-        <script>
-          function pay() {
-            // Change button text to indicate processing
-            const payButton = document.getElementById('payButton');
-            payButton.textContent = 'Processing your transaction...';
-            payButton.disabled = true;
-
-            // Simulate a delay before redirect
-            setTimeout(() => {
-              // Redirect back to the app using the deep link
-              const deepLink = 'custom.flutterbooksample.com://transaction/${transactionNumber}';
-              window.location.href = deepLink;
-            }, 2000);
-          }
-        </script>
+        <script>${JavaScript}</script>
       </body>
     </html>
-  `);
+  `;
+
+  // Render the financial institution page
+  res.send(HTML);
 });
 
 app.listen(PORT, () => {
