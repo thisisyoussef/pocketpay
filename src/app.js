5006a521f7ab988b2213f718a597d892dff40ca2
thisisyoussef <youssefahmed@ufl.edu>
2023-07-13 04:06:31 +0300

    updated institution page design

--- src/app.js
+++ src/app.js
@@ -1,102 +1,55 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+// React and ReactDOM are not available in Node.js
+// So, this is just a hypothetical example
+const React = require('react');
+const ReactDOMServer = require('react-dom/server');
+
 // Add body-parser middleware
 const app = express();
 app.use(bodyParser.json());
 
 const PORT = process.env.PORT || 3007;
 
-app.get('/paymentProcess', (req, res) => {
-  // Generate transaction details (dummy data)
+function TransactionPage() {
   const transactionNumber = '123456789';
   const amountToPay = 100.0;
 
-  if (!transactionNumber || !amountToPay) {
-    return res.status(400).send('Invalid transaction details.');
-  }
+  // onClick event is not applicable in server side rendering
+  // So, this is just a hypothetical example
+  const pay = () => {
+    const deepLink = `custom.flutterbooksample.com://transaction/${transactionNumber}`;
+    window.location.href = deepLink;
+  };
 
-  // Render the financial institution page
+  return React.createElement('main', {className: 'transaction-page'}, [
+    React.createElement('header', {}, [
+      React.createElement('h1', {}, 'Transaction Details')
+    ]),
+    React.createElement('section', {}, [
+      React.createElement('p', {}, `Transaction Number: ${transactionNumber}`),
+      React.createElement('p', {}, `Amount to Pay: $${amountToPay}`),
+      React.createElement('button', {onClick: pay}, 'Complete Transaction')
+    ])
+  ]);
+}
+
+app.get('/paymentProcess', (req, res) => {
+  // Generate the HTML string from the React component
+  const htmlString = ReactDOMServer.renderToString(React.createElement(TransactionPage));
+
+  // Send the HTML string back to the client
   res.send(`
     <html>
       <head>
         <title>Financial Institution</title>
         <style>
-          body {
-            font-family: Arial, sans-serif;
-            margin: 0;
-            padding: 0;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            height: 100vh;
-            background-color: #f0f0f0;
-            color: #333;
-          }
-
-          h1 {
-            color: #444;
-          }
-
-          p {
-            color: #444;
-            line-height: 1.5;
-          }
-
-          button {
-            padding: 15px 30px;
-            font-size: 20px;
-            background-color: #007BFF;
-            color: white;
-            border: none;
-            border-radius: 5px;
-            cursor: pointer;
-          }
-
-          button:hover {
-            background-color: #0056b3;
-          }
-
-          .container {
-            max-width: 600px;
-            margin: 0 auto;
-            padding: 20px;
-            box-sizing: border-box;
-          }
-
-          @media (max-width: 600px) {
-            .container {
-              padding: 10px;
-            }
-          }
+          /* Your CSS styles would go here */
         </style>
       </head>
       <body>
-        <header>
-          <h1>Transaction Details</h1>
-        </header>
-        <section class="container">
-          <p><strong>Transaction Number:</strong> ${transactionNumber}<br>
-          This is your unique identifier for this transaction.</p>
-          <p><strong>Amount to Pay:</strong> $${amountToPay}<br>
-          This is the total amount due for the transaction.</p>
-          <button id="payButton" onclick="pay()">Complete Transaction</button>
-        </section>
-        <script>
-          function pay() {
-            // Change button text to indicate processing
-            const payButton = document.getElementById('payButton');
-            payButton.textContent = 'Processing your transaction...';
-            payButton.disabled = true;
-
-            // Simulate a delay before redirect
-            setTimeout(() => {
-              // Redirect back to the app using the deep link
-              const deepLink = 'custom.flutterbooksample.com://transaction/${transactionNumber}';
-              window.location.href = deepLink;
-            }, 2000);
-          }
-        </script>
+        ${htmlString}
       </body>
     </html>
   `);
