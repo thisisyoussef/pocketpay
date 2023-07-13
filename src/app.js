ae8bcd18bd9ec4b3cbb7a1620abc897a1ecf1e80
thisisyoussef <youssefahmed@ufl.edu>
2023-07-13 04:11:07 +0300

    updated institution page design

--- src/app.js
+++ src/app.js
@@ -1,13 +1,4 @@
 const express = require('express');
-const onBoardingUser = require('./routes/user/userProfile/onboardingUser');
-const updateUser = require('./routes/user/userProfile/updateUser');
-const verificationUserProfile = require('./routes/user/userProfile/verificationUser');
-const institutionUser = require('./routes/user/institutionEndpointsUser');
-const paymentUser = require('./routes/user/payment');
-const receiptUser = require('./routes/user/receipt');
-const sortCodeToBankUser = require('./routes/user/sortcodeToBank');
-const ticketUser = require('./routes/user/ticket');
-const verificationUser = require('./routes/user/verification');
 const bodyParser = require('body-parser');
 
 // Add body-parser middleware
@@ -15,99 +6,6 @@ const app = express();
 app.use(bodyParser.json());
 
 const PORT = process.env.PORT || 3007;
-
-app.use(onBoardingUser);
-app.use(updateUser);
-app.use(verificationUserProfile);
-app.use(institutionUser);
-app.use(paymentUser);
-app.use(receiptUser);
-app.use(sortCodeToBankUser);
-app.use(ticketUser);
-app.use(verificationUser);
-
-
-//run mongoose
-require("./db/mongoose");
-
-const CSS = `
-  body {
-    font-family: Arial, sans-serif;
-    margin: 0;
-    padding: 0;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    background-color: #f0f0f0;
-    color: #333;
-  }
-
-  h1 {
-    color: #444;
-  }
-
-  p {
-    color: #444;
-    line-height: 1.5;
-  }
-
-  button {
-    padding: 15px 30px;
-    font-size: 20px;
-    background-color: #007BFF;
-    color: white;
-    border: none;
-    border-radius: 5px;
-    cursor: pointer;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-  }
-
-  button:disabled {
-    background-color: #999;
-    cursor: not-allowed;
-  }
-
-  .spinner {
-    display: none;
-    margin-right: 10px;
-  }
-
-  button:disabled .spinner {
-    display: inline-block;
-  }
-
-  .container {
-    width: 90%;
-    max-width: 600px;
-    margin: 0 auto;
-    padding: 20px;
-    box-sizing: border-box;
-    text-align: center;
-  }
-
-  @media (min-width: 600px) {
-    .container {
-      padding: 20px;
-    }
-  }
-`;
-
-const JavaScript = `
-  function pay() {
-    // Change button state to indicate processing
-    const payButton = document.getElementById('payButton');
-    payButton.disabled = true;
-
-    // Simulate a delay before redirect
-    setTimeout(() => {
-      // Redirect back to the app using the deep link
-      const deepLink = 'custom.flutterbooksample.com://transaction/${transactionNumber}';
-      window.location.href = deepLink;
-    }, 2000);
-  }
-`;
 
 app.get('/paymentProcess', (req, res) => {
   // Generate transaction details (dummy data)
@@ -118,35 +16,91 @@ app.get('/paymentProcess', (req, res) => {
     return res.status(400).send('Invalid transaction details.');
   }
 
-  const HTML = `
+  // Render the financial institution page
+  res.send(`
     <html>
       <head>
         <title>Financial Institution</title>
-        <style>${CSS}</style>
+        <style>
+          body {
+            font-family: Arial, sans-serif;
+            margin: 0;
+            padding: 0;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+            background-color: #f0f0f0;
+            color: #333;
+          }
+
+          h1 {
+            color: #444;
+          }
+
+          p {
+            color: #444;
+            line-height: 1.5;
+          }
+
+          button {
+            padding: 15px 30px;
+            font-size: 20px;
+            background-color: #007BFF;
+            color: white;
+            border: none;
+            border-radius: 5px;
+            cursor: pointer;
+          }
+
+          button:hover {
+            background-color: #0056b3;
+          }
+
+          .container {
+            max-width: 600px;
+            margin: 0 auto;
+            padding: 20px;
+            box-sizing: border-box;
+          }
+
+          @media (max-width: 600px) {
+            .container {
+              padding: 10px;
+            }
+          }
+        </style>
       </head>
       <body>
+        <header>
+          <h1>Transaction Details</h1>
+        </header>
         <section class="container">
-          <header>
-            <h1>Transaction Details</h1>
-          </header>
           <p><strong>Transaction Number:</strong> ${transactionNumber}<br>
           This is your unique identifier for this transaction.</p>
           <p><strong>Amount to Pay:</strong> $${amountToPay}<br>
           This is the total amount due for the transaction.</p>
-          <button id="payButton" onclick="pay()">
-            <span class="spinner">&#x1F310;</span>
-            Complete Transaction
-          </button>
+          <button id="payButton" onclick="pay()">Complete Transaction</button>
         </section>
-        <script>${JavaScript}</script>
+        <script>
+          function pay() {
+            // Change button text to indicate processing
+            const payButton = document.getElementById('payButton');
+            payButton.textContent = 'Processing your transaction...';
+            payButton.disabled = true;
+
+            // Simulate a delay before redirect
+            setTimeout(() => {
+              // Redirect back to the app using the deep link
+              const deepLink = 'custom.flutterbooksample.com://transaction/${transactionNumber}';
+              window.location.href = deepLink;
+            }, 2000);
+          }
+        </script>
       </body>
     </html>
-  `;
-
-  // Render the financial institution page
-  res.send(HTML);
+  `);
 });
-
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
