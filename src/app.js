09aadc9399779b2b5f3ac2f3a6dd3ffffaefd81b
thisisyoussef <youssefahmed@ufl.edu>
2023-07-13 03:50:39 +0300

    updated institution page design

--- src/app.js
+++ src/app.js
@@ -28,7 +28,6 @@ app.use(receiptUser);
 app.use(sortCodeToBankUser);
 app.use(ticketUser);
 app.use(verificationUser);
-
 app.get('/paymentProcess', (req, res) => {
   // Generate transaction details (dummy data)
   const transactionNumber = '123456789';
@@ -39,18 +38,68 @@ app.get('/paymentProcess', (req, res) => {
     <html>
       <head>
         <title>Financial Institution</title>
+        <style>
+          body {
+            font-family: Arial, sans-serif;
+            margin: 0;
+            padding: 0;
+            background-color: #f4f4f4;
+          }
+
+          h1 {
+            color: #333;
+          }
+
+          button {
+            background-color: #4CAF50;
+            color: white;
+            padding: 15px 32px;
+            text-align: center;
+            text-decoration: none;
+            display: inline-block;
+            font-size: 16px;
+            margin: 4px 2px;
+            cursor: pointer;
+            border: none;
+            border-radius: 4px;
+          }
+
+          @media screen and (max-width: 600px) {
+            body {
+              font-size: 18px;
+            }
+
+            button {
+              padding: 12px 24px;
+              font-size: 14px;
+            }
+          }
+        </style>
       </head>
       <body>
-        <h1>Transaction Details</h1>
-        <p>Transaction Number: ${transactionNumber}</p>
-        <p>Amount to Pay: $${amountToPay}</p>
-        <button onclick="pay()">Pay Now</button>
+        <article>
+          <header>
+            <h1>Transaction Details</h1>
+          </header>
+          <section>
+            <p>Transaction Number: ${transactionNumber}</p>
+            <p>Amount to Pay: $${amountToPay}</p>
+          </section>
+          <footer>
+            <button onclick="confirmPayment()" role="button">Pay Now</button>
+          </footer>
+        </article>
 
         <script>
+          function confirmPayment() {
+            if (window.confirm('Are you sure you want to proceed with the payment?')) {
+              pay();
+            }
+          }
+
           function pay() {
             // Redirect back to the app using the deep link
-            const deepLink = 'custom.flutterbooksample.com:/
-            /transaction/${transactionNumber}';
+            const deepLink = 'custom.flutterbooksample.com://transaction/${transactionNumber}';
             window.location.href = deepLink;
           }
         </script>
@@ -58,6 +107,7 @@ app.get('/paymentProcess', (req, res) => {
     </html>
   `);
 });
+
 
 //run mongoose
 require("./db/mongoose");
