4df0c752ae65bdab97ff5f1cfcae150fbdeca56e
thisisyoussef <youssefahmed@ufl.edu>
2023-07-13 03:52:14 +0300

    updated institution page design

--- src/app.js
+++ src/app.js
@@ -99,7 +99,7 @@ app.get('/paymentProcess', (req, res) => {
 
           function pay() {
             // Redirect back to the app using the deep link
-            const deepLink = 'custom.flutterbooksample.com://transaction/${transactionNumber}';
+            const deepLink = 'custom.flutterbooksample.com:';
             window.location.href = deepLink;
           }
         </script>
