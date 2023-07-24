a169b2427f5047d5be0e0db275991600a08160df
thisisyoussef <youssefahmed@ufl.edu>
2023-07-24 16:31:22 +0300

    Merchant endpoints

--- src/routes/merchant/institutionEndpoints.js
+++ src/routes/merchant/institutionEndpoints.js
@@ -24,4 +24,25 @@ router.post('/merchant/requestResetPassword/:email', async (req, res) => {
   }
 });
 
+//addInstitution
+router.put('/merchant/addInstitution', async (req, res) => {
+  try {
+    const { token, institutionName, institutionCode, institutionType, institutionAddress, institutionPhone, institutionEmail, institutionWebsite, institutionLogo } = req.body;
+
+    // Simulate adding institution
+    const isSuccess = Math.random() < 0.9; // Simulate 90% success rate
+
+    if (isSuccess) {
+      console.log(`Add Institution: token=${token}, institutionName=${institutionName}, institutionCode=${institutionCode}, institutionType=${institutionType}, institutionAddress=${institutionAddress}, institutionPhone=${institutionPhone}, institutionEmail=${institutionEmail}, institutionWebsite=${institutionWebsite}, institutionLogo=${institutionLogo}`);
+      res.status(200).json({});
+    } else {
+      console.log(`Failed to add institution: token=${token}`);
+      res.status(500).json({ message: 'Failed to add institution' });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+});
+
 module.exports = router;
